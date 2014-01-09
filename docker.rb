d2435efbb39267370e73e99d8e1d932eaaea9979
Mark Severson <markse@instructure.com>
2014-01-09 14:06:05 -0700

    docker 0.7.5

--- docker.rb
+++ docker.rb
@@ -4,11 +4,11 @@ class Docker < Formula
   homepage 'https://www.docker.io/'
 
   if MacOS.prefer_64_bit?
-    url 'https://get.docker.io/builds/Darwin/x86_64/docker-0.7.4.tgz'
-    sha1 '4503c6f762cdc8310e3bb43babbb7f90b45697f7'
+    url 'https://get.docker.io/builds/Darwin/x86_64/docker-0.7.5.tgz'
+    sha1 '3ea4948a757a48c31568bb808fc2e66967309cf4'
   else
-    url 'https://get.docker.io/builds/Darwin/i386/docker-0.7.4.tgz'
-    sha1 'b21bfc0eafddcadd40218c138ec4a4272adc7e93'
+    url 'https://get.docker.io/builds/Darwin/i386/docker-0.7.5.tgz'
+    sha1 '386ae8b0e93d59a97da1c353afa77b6b7c48176c'
   end
 
   depends_on :arch => :intel
